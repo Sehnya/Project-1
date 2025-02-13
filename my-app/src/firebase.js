a490718b34c7d4623250588dbe970356a69ca8ba
Sehnya <Sehnyaw@gmail.com>
2025-02-12 23:42:18 -0500

    protection from EVILLLLLLLLLLLLL

--- my-app/src/firebase.js
+++ my-app/src/firebase.js
@@ -3,7 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged, updateProfile, sendEmailVerification, sendPasswordResetEmail } from "firebase/auth"; // Import auth functions
 
 const firebaseConfig = {
-    // Your Firebase configuration (apiKey, authDomain, etc.)
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
